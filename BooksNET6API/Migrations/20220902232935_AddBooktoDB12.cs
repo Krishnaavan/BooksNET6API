0fa9dd3a230481a05dc2dfa4348908f4ebb648d4
Krishnaavan <kavanthsa@hotmail.com>
2022-09-02 21:50:06 -0400

    Add project files.

---
+++ BooksNET6API/Migrations/20220902232935_AddBooktoDB12.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksNET6API.Migrations
+{
+    public partial class AddBooktoDB12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BooksNET6API/Models/Entities/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksNET6API.Models.Entities
+{
+    public class Book
+    {
+        public Book()
+        {
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        [Required]
+        public string Genere { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        [Required]
+        public string PublisherName { get; set; }
+
+    }
+
+    
+}
